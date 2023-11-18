2405f70793694dd5f44af6f484c19d119a538871
xto10xadmin <xto10xproperties@gmail.com>
2023-11-18 22:45:01 +0530

    First Start
    First Start

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: false,
+        arrows: true,
+        dots: false,
+        prevArrow: $(".arrow-left"),
+        nextArrow: $(".arrow-right"),
+    });
+});
